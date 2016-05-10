367fe8c2a205fb1a04cf73d6e7f83f3bcbe6cd65
sneakerkg <xiaotj1990327@gmail.com>
2016-05-09 20:20:19 -0700

    Add assignment3, have bp problem on reshapew

---
+++ examples/cs231n/assignment3/cs231n/rnn_layers_minpy.py
@@ -0,0 +1,88 @@
+import minpy.numpy as np
+
+def affine_forward(x, w, b):
+  """
+  Computes the forward pass for an affine (fully-connected) layer.
+
+  The input x has shape (N, d_1, ..., d_k) and contains a minibatch of N
+  examples, where each example x[i] has shape (d_1, ..., d_k). We will
+  reshape each input into a vector of dimension D = d_1 * ... * d_k, and
+  then transform it to an output vector of dimension M.
+
+  Inputs:
+  - x: A numpy array containing input data, of shape (N, d_1, ..., d_k)
+  - w: A numpy array of weights, of shape (D, M)
+  - b: A numpy array of biases, of shape (M,)
+  
+  Returns a tuple of:
+  - out: output, of shape (N, M)
+  - cache: (x, w, b)
+  """
+  out = None
+  xshape = x.shape
+  x = x.reshape(xshape[0], -1)
+  out = x.dot(w) + b
+  cache = (x, w, b)
+  return out, cache
+
+def rnn_step_forward(x, prev_h, Wx, Wh, b):
+  """
+  Run the forward pass for a single timestep of a vanilla RNN that uses a tanh
+  activation function.
+
+  The input data has dimension D, the hidden state has dimension H, and we use
+  a minibatch size of N.
+
+  Inputs:
+  - x: Input data for this timestep, of shape (N, D).
+  - prev_h: Hidden state from previous timestep, of shape (N, H)
+  - Wx: Weight matrix for input-to-hidden connections, of shape (D, H)
+  - Wh: Weight matrix for hidden-to-hidden connections, of shape (H, H)
+  - b: Biases of shape (H,)
+
+  Returns a tuple of:
+  - next_h: Next hidden state, of shape (N, H)
+  - cache: Tuple of values needed for the backward pass.
+  """
+  next_h = np.tanh(np.dot(x, Wx) + np.dot(prev_h, Wh) + b)
+  cache = (x, prev_h, Wx, Wh, b, next_h)
+  return next_h, cache
+
+def rnn_forward(x, h0, Wx, Wh, b):
+  """
+  Run a vanilla RNN forward on an entire sequence of data. We assume an input
+  sequence composed of T vectors, each of dimension D. The RNN uses a hidden
+  size of H, and we work over a minibatch containing N sequences. After running
+  the RNN forward, we return the hidden states for all timesteps.
+  
+  Inputs:
+  - x: Input data for the entire timeseries, of shape (N, T, D).
+  - h0: Initial hidden state, of shape (N, H)
+  - Wx: Weight matrix for input-to-hidden connections, of shape (D, H)
+  - Wh: Weight matrix for hidden-to-hidden connections, of shape (H, H)
+  - b: Biases of shape (H,)
+  
+  Returns a tuple of:
+  - h: Hidden states for the entire timeseries, of shape (N, T, H).
+  - cache: Values needed in the backward pass
+  """
+  # get shapes
+  N,T,D = x.shape
+  _,H = h0.shape
+  # init result
+  h = np.zeros([N,0,H])
+  cache = []
+  for t in range(T):
+    if t == 0:
+      h_prev = h0
+    else:
+      h_prev = h[:,t-1,:].reshape([N,H])
+    x_t = x[:,t,:].reshape([N,D])
+    h_t, cache_t = rnn_step_forward(x_t, h_prev, Wx, Wh, b) 
+    h = np.append(h, h_t.reshape([N,1,H]), axis = 1)
+    cache.append(cache_t)
+  return h, cache
+
+
+
+
---
+++ examples/cs231n/assignment3/exp_rnn_minpy.py
@@ -0,0 +1,168 @@
+from cs231n.rnn_layers_minpy import *
+
+import numpy as np
+
+import minpy 
+import minpy.numpy as minpy_np
+import minpy.core
+import minpy.array
+from minpy.array_variants import ArrayType
+import minpy.dispatch.policy as policy
+from minpy.core import grad_and_loss
+
+from cs231n.gradient_check import eval_numerical_gradient, eval_numerical_gradient_array
+
+def NumpyVarToMinpy(var):
+  return minpy.array.Value.wrap(var)
+
+def MinpyVarToNumpy(var):
+  return minpy.array.Value.wrap(var).get_data(ArrayType.NUMPY)
+
+def rel_error(x, y):
+  a = np.max(np.abs(x - y) / (np.maximum(1e-5, np.abs(x) + np.abs(y))))
+  '''
+  if a > 0.5:
+    print 'probably error'
+    print np.abs(x - y) / (np.maximum(1e-5, np.abs(x) + np.abs(y)))
+    print 'left'
+    print x 
+    print 'right'
+    print y 
+  '''
+  return a
+
+# Forward step
+def Test_Forward_Step():
+  N, D, H = 3, 10, 4
+
+  x = np.linspace(-0.4, 0.7, num=N*D).reshape(N, D)
+  prev_h = np.linspace(-0.2, 0.5, num=N*H).reshape(N, H)
+  Wx = np.linspace(-0.1, 0.9, num=D*H).reshape(D, H)
+  Wh = np.linspace(-0.3, 0.7, num=H*H).reshape(H, H)
+  b = np.linspace(-0.2, 0.4, num=H)
+
+  mp_x = NumpyVarToMinpy(x)
+  mp_prev_h = NumpyVarToMinpy(prev_h)
+  mp_Wx = NumpyVarToMinpy(Wx)
+  mp_Wh = NumpyVarToMinpy(Wh)
+  mp_b = NumpyVarToMinpy(b)
+  
+  mp_next_h, _ = rnn_step_forward(mp_x, mp_prev_h, mp_Wx, mp_Wh, mp_b)
+  next_h = MinpyVarToNumpy(mp_next_h)
+  
+  expected_next_h = np.asarray([[-0.58172089, -0.50182032, -0.41232771, -0.31410098],[ 0.66854692,  0.79562378,  0.87755553,  0.92795967],[ 0.97934501,  0.99144213,  0.99646691,  0.99854353]])
+
+  print 'next_h error: ', rel_error(expected_next_h, next_h)
+
+def Test_Backward_Step():
+  N, D, H = 4, 5, 6
+  x = np.random.randn(N, D)
+  h = np.random.randn(N, H)
+  Wx = np.random.randn(D, H)
+  Wh = np.random.randn(H, H)
+  b = np.random.randn(H)
+
+  mp_x = NumpyVarToMinpy(x)
+  mp_h = NumpyVarToMinpy(h)
+  mp_Wx = NumpyVarToMinpy(Wx)
+  mp_Wh = NumpyVarToMinpy(Wh)
+  mp_b = NumpyVarToMinpy(b)
+
+  mp_next_h, _ = rnn_step_forward(mp_x, mp_h, mp_Wx, mp_Wh, mp_b)
+  out = MinpyVarToNumpy(mp_next_h)
+
+  dnext_h = np.random.randn(*out.shape)
+  mp_dnext_h = NumpyVarToMinpy(dnext_h)
+
+  rnn_step_forward_with_loss = lambda mp_x, mp_h, mp_Wx, mp_Wh, mp_b, mp_dnext_h: rnn_step_forward(mp_x, mp_h, mp_Wx, mp_Wh, mp_b)[0] * mp_dnext_h  
+ 
+  grad_function = grad_and_loss(rnn_step_forward_with_loss, range(0,5))
+
+  grad_arrays, loss = grad_function(mp_x, mp_h, mp_Wx, mp_Wh, mp_b, mp_dnext_h)
+
+  dx_num = eval_numerical_gradient_array(lambda x: MinpyVarToNumpy(rnn_step_forward(NumpyVarToMinpy(x), h, Wx, Wh, b)[0]), x, dnext_h)
+  dh_num = eval_numerical_gradient_array(lambda h: MinpyVarToNumpy(rnn_step_forward(x, NumpyVarToMinpy(h), Wx, Wh, b)[0]), h, dnext_h)
+  dWx_num = eval_numerical_gradient_array(lambda Wx: MinpyVarToNumpy(rnn_step_forward(x, h, NumpyVarToMinpy(Wx), Wh, b)[0]), Wx, dnext_h)
+  dWh_num = eval_numerical_gradient_array(lambda Wh: MinpyVarToNumpy(rnn_step_forward(x, h, Wx, NumpyVarToMinpy(Wh), b)[0]), Wh, dnext_h)
+  db_num = eval_numerical_gradient_array(lambda b: MinpyVarToNumpy(rnn_step_forward(x, h, Wx, Wh, NumpyVarToMinpy(b))[0]), b, dnext_h)
+
+  
+  print 'dx error: ', rel_error(dx_num, MinpyVarToNumpy(grad_arrays[0]))
+  print 'dh error: ', rel_error(dh_num, MinpyVarToNumpy(grad_arrays[1]))
+  print 'dWx error: ', rel_error(dWx_num, MinpyVarToNumpy(grad_arrays[2]))
+  print 'dWh error: ', rel_error(dWh_num, MinpyVarToNumpy(grad_arrays[3]))
+  print 'db error: ', rel_error(db_num, MinpyVarToNumpy(grad_arrays[4]))
+
+def Test_RNN_Forward():
+  N, T, D, H = 2, 3, 4, 5
+
+  x = np.linspace(-0.1, 0.3, num=N*T*D).reshape(N, T, D)
+  h0 = np.linspace(-0.3, 0.1, num=N*H).reshape(N, H)
+  Wx = np.linspace(-0.2, 0.4, num=D*H).reshape(D, H)
+  Wh = np.linspace(-0.4, 0.1, num=H*H).reshape(H, H)
+  b = np.linspace(-0.7, 0.1, num=H)
+
+  mp_x = NumpyVarToMinpy(x)
+  mp_h0 = NumpyVarToMinpy(h0)
+  mp_Wx = NumpyVarToMinpy(Wx)
+  mp_Wh = NumpyVarToMinpy(Wh)
+  mp_b = NumpyVarToMinpy(b)
+
+  h, _ = rnn_forward(mp_x, mp_h0, mp_Wx, mp_Wh, mp_b)
+ 
+  expected_h = np.asarray([
+      [
+            [-0.42070749, -0.27279261, -0.11074945,  0.05740409,  0.22236251],
+                [-0.39525808, -0.22554661, -0.0409454,   0.14649412,  0.32397316],
+                    [-0.42305111, -0.24223728, -0.04287027,  0.15997045,  0.35014525],
+                      ],
+        [
+              [-0.55857474, -0.39065825, -0.19198182,  0.02378408,  0.23735671],
+                  [-0.27150199, -0.07088804,  0.13562939,  0.33099728,  0.50158768],
+                      [-0.51014825, -0.30524429, -0.06755202,  0.17806392,  0.40333043]]])
+  
+  print 'h error: ', rel_error(expected_h, h)
+
+def Test_RNN_Backward():
+  N, D, T, H = 2, 3, 10, 5
+
+  x = np.random.randn(N, T, D)
+  h = np.random.randn(N, H)
+  Wx = np.random.randn(D, H)
+  Wh = np.random.randn(H, H)
+  b = np.random.randn(H)
+
+  mp_x = NumpyVarToMinpy(x)
+  mp_h = NumpyVarToMinpy(h)
+  mp_Wx = NumpyVarToMinpy(Wx)
+  mp_Wh = NumpyVarToMinpy(Wh)
+  mp_b = NumpyVarToMinpy(b)
+
+  out, _ = rnn_forward(mp_x, mp_h, mp_Wx, mp_Wh, mp_b)
+
+  dout = np.random.randn(*MinpyVarToNumpy(out).shape)
+  mp_dout = NumpyVarToMinpy(dout)
+
+  rnn_forward_with_loss = lambda mp_x, mp_h, mp_Wx, mp_Wh, mp_b, mp_out: rnn_forward(mp_x, mp_h, mp_Wx, mp_Wh, mp_b)[0] * mp_out 
+  
+  grad_function = grad_and_loss(rnn_forward_with_loss, range(0,5))
+
+  grad_arrays, loss = grad_function(mp_x, mp_h, mp_Wx, mp_Wh, mp_b, mp_dout)
+
+  dx_num = eval_numerical_gradient_array(lambda x: MinpyVarToNumpy(rnn_forward(NumpyVarToMinpy(x), h, Wx, Wh, b)[0]), x, dout)
+  dh_num = eval_numerical_gradient_array(lambda h: MinpyVarToNumpy(rnn_forward(x, NumpyVarToMinpy(h), Wx, Wh, b)[0]), h, dout)
+  dWx_num = eval_numerical_gradient_array(lambda Wx: MinpyVarToNumpy(rnn_forward(x, h, NumpyVarToMinpy(Wx), Wh, b)[0]), Wx, dout)
+  dWh_num = eval_numerical_gradient_array(lambda Wh: MinpyVarToNumpy(rnn_forward(x, h, Wx, NumpyVarToMinpy(Wh), b)[0]), Wh, dout)
+  db_num = eval_numerical_gradient_array(lambda b: MinpyVarToNumpy(rnn_forward(x, h, Wx, Wh, NumpyVarToMinpy(b))[0]), b, dout)
+  
+  print 'dx error: ', rel_error(dx_num, MinpyVarToNumpy(grad_arrays[0]))
+  print 'dh error: ', rel_error(dh_num, MinpyVarToNumpy(grad_arrays[1]))
+  print 'dWx error: ', rel_error(dWx_num, MinpyVarToNumpy(grad_arrays[2]))
+  print 'dWh error: ', rel_error(dWh_num, MinpyVarToNumpy(grad_arrays[3]))
+  print 'db error: ', rel_error(db_num, MinpyVarToNumpy(grad_arrays[4]))
+
+
+#Test_Forward_Step()
+#Test_Backward_Step()
+#Test_RNN_Forward()
+Test_RNN_Backward()
